f9d0454eacfcde713940dbc06918e57fd074d093
FullstackDevelopment <ankitkr756208@gmail.com>
2026-02-18 11:49:10 +0530

    first commit

---
+++ js_practice/sync_async.js
@@ -0,0 +1,12 @@
+console.log("we are learning sync and async js")
+//sync
+console.log("start 1")
+console.log("start 2")
+console.log("start 3")
+//async
+console.log("ram")
+
+setTimeout(()=>{
+    console.log("ankit")
+},2000)
+console.log("saidpur")
